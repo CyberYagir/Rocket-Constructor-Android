f3b91441f0b3c23299e781611e8dc0ff008b1d10
TupiDebil <yagirman59@gmail.com>
2021-03-28 13:26:41 +0300

    Add turret, simulation, explode

---
+++ Assets/TurretHandle.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretHandle : MonoBehaviour
+{
+    public Transform yHandle;
+    public SpriteRenderer hand;
+    public GameObject mainRocket;
+    private void Update()
+    {
+        yHandle.gameObject.transform.position = new Vector3(yHandle.gameObject.transform.position.x, mainRocket.transform.position.y, yHandle.gameObject.transform.position.z);
+        hand.size = new Vector2(Vector2.Distance(yHandle.transform.position, mainRocket.transform.position), 0.45f);
+    }
+}
--- Assets/UIManager.cs
+++ Assets/UIManager.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class UIManager : MonoBehaviour
 {
     public Animator shop;
+    public GameObject turret;
     public void ShowShop()
     {
         shop.Play("Show");
@@ -13,5 +14,16 @@ public class UIManager : MonoBehaviour
     public void HideShop()
     {
         shop.Play("Hide");
+    }
+
+
+    public void StartSimulation()
+    {
+        var rocket = GameObject.FindGameObjectWithTag("MainPart");
+        rocket.SetActive(false);
+        GameObject simRocket = Instantiate(rocket.gameObject, rocket.transform.position, rocket.transform.rotation);
+        simRocket.SetActive(true);
+        turret.gameObject.SetActive(false);
+        simRocket.AddComponent<Rigidbody2D>();
     }
 }
