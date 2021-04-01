1050776988fa1316db2e5396e5aa897303e67ec6
TupiDebil <yagirman59@gmail.com>
2021-04-01 18:30:41 +0300

    Stable 0.1

---
+++ Assets/Player.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float money = 1000000;
+
+    public string companyName = "NoName";
+
+
+
+
+}
