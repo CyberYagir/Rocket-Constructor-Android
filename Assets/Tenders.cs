8e3ea56fd126a7253c0c76a052f9a1790e104335
TupiDebil <yagirman59@gmail.com>
2021-04-02 14:12:36 +0300

    Tenders

--- Assets/Tenders.cs
+++ Assets/Tenders.cs
@@ -2,17 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+public class Company
+{
+    public string name;
+    public int peoples;
+    
+};
+
+public class Tender { 
+    //public 
+}
+
+
+
 public class Tenders : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
