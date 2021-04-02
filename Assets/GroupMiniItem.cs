50727694b042cd048ab58cafa53da85531ad0503
TupiDebil <yagirman59@gmail.com>
2021-04-02 18:45:20 +0300

    Tenders Companies + Parts

--- Assets/GroupMiniItem.cs
+++ Assets/GroupMiniItem.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
@@ -11,14 +12,35 @@ public class GroupMiniItem : MonoBehaviour, IPointerDownHandler
 
     public void OnPointerDown(PointerEventData eventData)
     {
+        if (GetComponentInParent<MiniGroups>().group != null)
+        {
+            for (int i = 0; i < GetComponentInParent<MiniGroups>().group.parts.Count; i++)
+            {
+                var n = FindObjectsOfType<Part>().ToList().Find(x => x.partFullName == GetComponentInParent<MiniGroups>().group.parts[i].partFullName);
+                if (n != null)
+                {
+                    n.GetComponent<SpriteRenderer>().color = Color.white;
+                }
+            }
+        }
         GetComponentInParent<MiniGroups>().group = group;
         if (GetComponentInParent<MiniGroups>().button == gameObject)
         {
+
             GetComponentInParent<MiniGroups>().menu.active = !GetComponentInParent<MiniGroups>().menu.active;
+
         }
         else
         {
             GetComponentInParent<MiniGroups>().menu.active = true;
+        }
+        for (int i = 0; i < group.parts.Count; i++)
+        {
+            var n = FindObjectsOfType<Part>().ToList().Find(x => x.partFullName == group.parts[i].partFullName);
+            if (n != null)
+            {
+                n.GetComponent<SpriteRenderer>().color = GetComponentInParent<MiniGroups>().menu.active ? Color.green : Color.white;
+            }
         }
         GetComponentInParent<MiniGroups>().button = gameObject;
     }
--- Assets/Tenders.cs
+++ Assets/Tenders.cs
@@ -3,20 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
+[System.Serializable]
 public class Company
 {
     public string name;
     public int peoples;
-    
-};
-
-public class Tender { 
-    //public 
+    public Sprite icon;
+}
+[System.Serializable]
+public class Tender {
+    public Company company;
+    public int money;
+    public GameObject prefab;
+    public Planet planet;
+    [TextArea]
+    public string text;
 }
 
 
 
 public class Tenders : MonoBehaviour
 {
-    
+    public Planet[] planets;
+    public Company[] companies;
+    public GameObject[] sattelites;
+
+    public List<Tender> tenders;
 }
