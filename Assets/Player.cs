dbf271e92fa5403a4c9cb94663c8ff221e607ca4
TupiDebil <yagirman59@gmail.com>
2021-04-03 12:39:10 +0300

    Ads

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Advertisements;
 
 public class Player : MonoBehaviour
 {
@@ -8,7 +9,18 @@ public class Player : MonoBehaviour
 
     public static string companyName = "NoName";
 
+    void Start(){
+        Advertisement.Initialize("4074573", false);
 
+    }
     
-
+    public void ShowInterstitialAd() {
+        // Check if UnityAds ready before calling Show method:
+        if (Advertisement.IsReady()) {
+            Advertisement.Show("video");
+        } 
+        else {
+            Debug.Log("Interstitial ad not ready at the moment! Please try again later!");
+        }
+    }
 }
--- Assets/Tenders.cs
+++ Assets/Tenders.cs
@@ -1,8 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-
 [System.Serializable]
 public class Company
 {
@@ -19,14 +17,34 @@ public class Tender {
     [TextArea]
     public string text;
 }
-
-
-
 public class Tenders : MonoBehaviour
 {
     public Planet[] planets;
     public Company[] companies;
     public GameObject[] sattelites;
-
     public List<Tender> tenders;
+    [Space]
+    public Transform holder, item;
+    void Start(){
+        for(int i = 0; i < planets.Length; i++){
+            tenders.Add(new Tender(){company = companies[Random.Range(0, companies.Length)], money = ((int)planets[i].minY * 100) + (int)(1000000 * Random.value), prefab = sattelites[Random.Range(0, sattelites.Length)], planet = planets[Random.Range(0, planets.Length)]});
+        }
+
+        for (int i = 0; i < Random.Range(5,10); i++)
+        {
+            var pl = planets[Random.Range(0, planets.Length)];
+            tenders.Add(new Tender(){company = companies[Random.Range(0, companies.Length)], money = ((int)pl.minY * 100) + (int)(1000000f * Random.value), prefab = sattelites[Random.Range(0, sattelites.Length)], planet = pl});
+        }
+
+    }
+
+    public void UpdateList(){
+        foreach (Transform item in holder)
+        {
+            Destroy(item.gameObject);   
+        }
+
+        greger
+    }
+
 }
